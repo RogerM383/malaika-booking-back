eae918baacea166b7c6e96954d72d806033d4bb1
meuk <dev1@metodo10.com>
2023-07-14 01:29:55 +0200

    Add slugeable trait

---
+++ app/Traits/Slugeable.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Traits;
+
+trait Slugeable
+{
+    public static function slugify($text, string $divider = '-')
+    {
+        $text = preg_replace('~[^\pL\d]+~u', $divider, $text);
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+        $text = preg_replace('~[^-\w]+~', '', $text);
+        $text = trim($text, $divider);
+        $text = preg_replace('~-+~', $divider, $text);
+        $text = strtolower($text);
+
+        if (empty($text)) {
+            return 'n-a';
+        }
+
+        return $text;
+    }
+}
