aeadece591b3d6fea8c8cfbfa845695b89551ba9
meuk <dev1@metodo10.com>
2023-11-23 17:05:52 +0100

    log for headers

--- app/Http/Middleware/SetJsonContentType.php
+++ app/Http/Middleware/SetJsonContentType.php
@@ -4,12 +4,15 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class SetJsonContentType
 {
     public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
+
+        Log::debug(json_encode($request->header()));
 
         // Asegúrate de que la respuesta sea JSON y establece el encabezado Content-Type si no está presente.
         if (!$response->headers->has('Content-Type')) {
