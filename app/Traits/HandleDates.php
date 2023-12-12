3706602d22763e3cb262596a03614b0b7f3d667e
meuk <dev1@metodo10.com>
2023-12-12 20:27:09 +0100

    fix months on mails

--- app/Traits/HandleDates.php
+++ app/Traits/HandleDates.php
@@ -3,6 +3,21 @@ namespace App\Traits;
 
 use Illuminate\Support\Carbon;
 
+const MESOS = [
+    'Gener',
+    'Febrer',
+    'Març',
+    'Abril',
+    'Maig',
+    'Juny',
+    'Juliol',
+    'Agost',
+    'Setembre',
+    'Octubre',
+    'Novembre',
+    'Desembre'
+];
+
 trait HandleDates
 {
     function getPeriod ($start, $end)
@@ -10,9 +25,9 @@ trait HandleDates
         $_start = Carbon::createFromDate($start);
         $_end = Carbon::createFromDate($end);
         if ($_start->month === $_end->month) {
-            return 'del '.$_start->day.' al '.$_end->day.' de '.$_start->month;
+            return 'del '.$_start->day.' al '.$_end->day.' de '.MESOS[$_start->month-1];
         } else {
-            return 'del '.$_start->day.' de '.$_start->month.' al '.$_end->day.' de '.$_end->month;
+            return 'del '.$_start->day.' de '.MESOS[$_start->month-1].' al '.$_end->day.' de '.MESOS[$_end->month-1];
         }
     }
 }
